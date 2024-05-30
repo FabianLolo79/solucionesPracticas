90779f7eae4625c6125f97f5ed2ae057076c1f34
Fabián E. García <garciafabianenrique79@gmail.com>
2024-05-30 16:07:26 -0300

    ejercicio3 - estructura de datos_finalizado

--- EstructurasDeDatos/estructuras_de_datos_iniciando.py
+++ EstructurasDeDatos/estructuras_de_datos_iniciando.py
@@ -21,49 +21,82 @@ Nota: para saber si el número i es par hacer i % 2 == 0
 """
 
 # Ejercicio 1
-print("")
-print("Ejercicio 1")
-print("")
-lista_nombres = []
+"""
+Pedir al usuario que ingrese 10 nombres de personas (input en un ciclo) no repetidas,
+guardarlos en una lista y mostrarlos. 
+"""
+# print("")
+# print("Ejercicio 1")
+# print("")
+# lista_nombres = []
 
-nombre = input("Ingrese un nombre: ")
+# nombre = input("Ingrese un nombre: ")
 
-while len(lista_nombres) < 10:
+# while len(lista_nombres) < 10:
         
-        if nombre not in lista_nombres:
-            lista_nombres.append(nombre)
-        nombre = input("Ingrese un nombre: ")
+#         if nombre not in lista_nombres:
+#             lista_nombres.append(nombre)
+#         nombre = input("Ingrese un nombre: ")
 
-print("")
-print("")
-print(lista_nombres)
-print("")
-print("")
+# print("")
+# print("")
+# print(lista_nombres)
+# print("")
+# print("")
 
 
 # Ejercicio 2
-print("Ejercicio 2")
+"""
+Eliminar la tercer y la última persona de la lista del ejercicio 1, luego ordenar la lista y mostrarlo.
+"""
+# print("Ejercicio 2")
 
-lista_nombres.pop(2)
-print(f"Borrado del elemento 3 {lista_nombres}")
-print("")
-print("")
+# lista_nombres.pop(2)
+# print(f"Borrado del elemento 3 {lista_nombres}")
+# print("")
+# print("")
 
-lista_nombres.pop(-1)
-print(f"Borrado del último elemento {lista_nombres}")
-print("")
-print("")
+# lista_nombres.pop(-1)
+# print(f"Borrado del último elemento {lista_nombres}")
+# print("")
+# print("")
 
-lista_nombres.sort()
-print(f"Ordenado alfabeticamente {lista_nombres}")
-print("")
-print("")
+# lista_nombres.sort()
+# print(f"Ordenado alfabeticamente {lista_nombres}")
+# print("")
+# print("")
 
-print("")
-print("")
+# print("")
+# print("")
 
 
 # Ejercicio 3
-#algun cambio bla bla bla
+"""
+Guardar en un diccionario los datos de una persona: nombre, apellido, dni, email, fecha de nacimiento.
+"""
+persona = {}
+
+nombre = input("Ingrese el nombre: \n")
+persona["Nombre"] = nombre
+
+apellido = input("Ingrese el apellido: \n")
+persona["Apellido"] = apellido
+
+dni = int(input("Ingrese el dni (sin puntos): \n"))
+persona["Dni"] = dni
+
+email = input("Ingrese el e-mail: \n")
+persona["E-mail"] = email
+
+fecha_nac = input("Ingrese la fecha de nacimiento \n")
+persona["Fecha de nacimiento"] = fecha_nac
+
+print(persona)
+
+# Ejercicio 4
+"""
+En un nuevo diccionario llamado familia guardar 4 personas, cada una de ellas con los mismos 5 datos 
+(nombre, apellido, dni, email, fecha de nacimiento).
+"""
 
 
